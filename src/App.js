244900f9e3ece03b4f16bd2e217b8bfc23dfb5d5
camposLuis <luis.campos.henrique@hotmail.com>
2019-09-03 00:15:46 -0300

    Utilizando o Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [tech, setTech] = useState(['ReactJS', 'React Native']);
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
     setTech([...tech, newTech]);
     setNewTech('');
   }
+
+  /**
+   * Substitui o componentDidMount
+   */
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  /**
+   * Substitui o componentDidUpdate
+   */
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
 
   return (
     <>
